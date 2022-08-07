360a6e7118db97ef6eb1f6ecb2a372fd5ddf312d
TTHai-Git <spaceboy534@gmail.com>
2022-08-07 16:29:33 +0700

    Update 3

--- Gototop.js
+++ Gototop.js
@@ -1,9 +1,23 @@
-const btnScrollToTop = document.querySelector("#btnScrollToTop")
-btnScrollToTop.addEventListener("click",function(){
-    // window.scrollTo(0, 0);
-    window.scrollTo({
-        top:0,
-        left:0,
-        behavior:"smooth"
+$(document).ready(function() {
+
+    $("#btnScrollToTop").hide()
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop()>= 100 ) {
+            $("#btnScrollToTop").show("slow")
+        }
+        else
+        {
+            $("#btnScrollToTop").hide("slow")
+        }
     })
-})      
+    const btnScrollToTop = document.querySelector("#btnScrollToTop")
+    btnScrollToTop.addEventListener("click",function(){
+    // window.scrollTo(0, 0);
+        window.scrollTo({
+            top:0,
+            left:0,
+            behavior:"smooth"
+        })
+    })      
+})
